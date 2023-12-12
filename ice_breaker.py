5b96c6d081fae59c64fe52990a5367297c3fa6e8
ganapathya <ganapathya@gmail.com>
2023-12-12 13:22:52 +0530

    adding in the initial skeleton files

---
+++ ice_breaker.py
@@ -0,0 +1,8 @@
+from langchain import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.chat_models import ChatGooglePalm
+from langchain.chains import LLMChain
+
+if __name__ == '__main__':
+    print("hello langchain!")
+    
